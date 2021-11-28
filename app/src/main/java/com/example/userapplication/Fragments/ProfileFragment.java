6af10bf6851a97eece2683a67d96ab0d57fdd795
CoconutWave <rv_nugroho25@yahoo.co.id>
2021-11-28 21:11:14 +0700

    Added UserApp class, login bisa bawa data user, added EditProfileActivity (belum selesai), minor UI fix
    Untuk login dengan data bisa ambil BaseController terbaru dari folder MDPService yang ada di Digital-Menu (app buat admin)

--- app/src/main/java/com/example/userapplication/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/userapplication/Fragments/ProfileFragment.java
@@ -1,13 +1,20 @@
 package com.example.userapplication.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.userapplication.Classes.UserApp;
+import com.example.userapplication.EditProfileActivity;
 import com.example.userapplication.R;
 
 /**
@@ -18,21 +25,21 @@ import com.example.userapplication.R;
 public class ProfileFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
-//    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM1 = "param1";
 //    private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-//    private String mParam1;
+    private UserApp loggedIn;
 //    private String mParam2;
 
     //SEMUA VARIABEL SEMENTARA DIREMARK, SAAT INI BELUM DIPAKAI
 
 
     // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
+    public static ProfileFragment newInstance(UserApp loggedIn) {
         ProfileFragment fragment = new ProfileFragment();
         Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
+        args.putParcelable(ARG_PARAM1, loggedIn);
 //        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
@@ -42,7 +49,7 @@ public class ProfileFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
+            loggedIn = getArguments().getParcelable(ARG_PARAM1);
 //            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
@@ -52,5 +59,29 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    TextView name, email, cash, phone;
+    Button btnEdit;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        name = view.findViewById(R.id.tv_profilename);
+        email = view.findViewById(R.id.tv_profileemail);
+        cash = view.findViewById(R.id.tv_profilesaldo);
+        phone = view.findViewById(R.id.tv_profilephonenum);
+        btnEdit = view.findViewById(R.id.btn_profileedit);
+        name.setText(loggedIn.getName());
+        email.setText(loggedIn.getEmail());
+        cash.setText("Rp "+String.format("%,.2f", new Double(loggedIn.getSaldo())));
+        phone.setText(loggedIn.getTelp());
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent edit = new Intent(getActivity(), EditProfileActivity.class);
+                edit.putExtra("loggedIn",loggedIn);
+                startActivity(edit);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/userapplication/HomeActivity.java
+++ app/src/main/java/com/example/userapplication/HomeActivity.java
@@ -17,6 +17,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.userapplication.Classes.UserApp;
 import com.example.userapplication.Fragments.HomeFragment;
 import com.example.userapplication.Fragments.OrderFragment;
 import com.example.userapplication.Fragments.ProfileFragment;
@@ -33,12 +34,17 @@ import java.util.Map;
 
 public class HomeActivity extends AppCompatActivity {
 
+    UserApp loggedIn;
     BottomNavigationView navbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         navbar = findViewById(R.id.nav_home);
+        Intent par = getIntent();
+        if(par.hasExtra("loggedIn")){
+            loggedIn = par.getParcelableExtra("loggedIn");
+        }
 
         navbar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
@@ -58,7 +64,7 @@ public class HomeActivity extends AppCompatActivity {
                         frag = OrderFragment.newInstance();
                         break;
                     case R.id.btn_navprofile:
-                        frag = ProfileFragment.newInstance("param1", "param2");
+                        frag = ProfileFragment.newInstance(loggedIn);
                         break;
                 }
                 try {
